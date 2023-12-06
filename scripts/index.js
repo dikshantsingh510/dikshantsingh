cc42c953a2ad751299342d33e7ad36a8f13460be
Dikshant Singh <dikshantsingh510@gmail.com>
2023-12-06 21:44:47 +0530

    Delete scripts directory

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-let menu_btn = document.getElementById("menu_btn");
-let navbar = document.querySelector("nav");
-
-menu_btn.addEventListener("click",(e)=>{
-    navbar.classList.toggle("active")
-    // console.log(navbar.classList);
-    // console.log(e.target);
-})
